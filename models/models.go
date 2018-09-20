8354a3bbc4df238495be82662ff3ffd2b6e98fcf
ali.soltani <arabshahi.soltani@gmail.com>
2018-09-20 17:34:50 +0430

    chang 3dmodel.add json tag for all field

--- models/models.go
+++ models/models.go
@@ -6,55 +6,54 @@ import "time"
 
 // Media type.
 type Media struct {
-	Title string
-	Type  string // gif, png, mp4, ...
-	Path  string
+	Title string `json:"title"`
+	Type  string `json:"type"` // gif, png, mp4, ...
+	Path  string `json:"path"`
 }
 
-// Model3D type.
+// Model3D type.TODO:sepehr ino ye check bokon.age benazaret in ziadi karie esme texture ro avaz konam,masaln beshe meta.
+// bad kolan be media ezafe she.to halate mamoli khali mimone vali vagti object 3d bashe ba texture por mishe
 type Model3D struct {
-	Title        string
-	ObjectPath   string
-	TexturesPath []struct {
-		Path      string
-		Thumbnail string
-	}
+	Title    string  `json:"title"`
+	Type     string  `json:"type"` // gif, png, mp4, ...
+	Path     string  `json:"path"`
+	Textures []Media `json:"textures"`
 }
 
 // Address type.
 type Address struct {
-	Country     string
-	City        string
-	Value       string
-	PhoneNumber string
-	PostCode    string
-	IsDefault   bool
+	Country     string `json:"country"`
+	City        string `json:"city"`
+	Value       string `json:"value"`
+	PhoneNumber string `json:"phone_number"`
+	PostCode    string `json:"post_code"`
+	IsDefault   bool   `json:"is_default"`
 }
 
 //BankAccount type
 type BankAccount struct {
-	Bank          string
-	Ownername     string
-	AccountNumber string
+	Bank          string `json:"bank"`
+	OwnerName     string `json:"owner_name"`
+	AccountNumber string `json:"account_number"`
 }
 
 //Discount model //TODO: ????
 type Discount struct {
-	ID         int `sql:",AUTO_INCREMENT" json:"id"`
-	Percentage int
-	ValidTime  time.Time
+	ID         int       `sql:",AUTO_INCREMENT" json:"id"`
+	Percentage int       `json:"percentage"`
+	ValidTime  time.Time `json:"valid_time"`
 }
 
 //******************************************************************************
 
 //SMS model.
 type SMS struct {
-	ID          int64 `sql:",AUTO_INCREMENT" json:"id"`
-	Code        int
-	UUID        string
-	PhoneNumber string
-	RequestDate time.Time
-	ExpiryDate  time.Time
+	ID          int64     `sql:",AUTO_INCREMENT" json:"id"`
+	Code        int       `json:"code"`
+	UUID        string    `json:"uuid"`
+	PhoneNumber string    `json:"phone_number"`
+	RequestDate time.Time `json:"request_date"`
+	ExpiryDate  time.Time `json:"expiry_date"`
 }
 
 //******************************************************************************
@@ -62,10 +61,10 @@ type SMS struct {
 // AppInfo is used to keep the latest version of different components of the
 // system in database such as app version or backend version.AppInfo.
 type AppInfo struct {
-	ID          int64 `sql:",AUTO_INCREMENT" json:"id"`
-	Name        string
-	Version     string
-	IsSupported bool
+	ID          int64  `sql:",AUTO_INCREMENT" json:"id"`
+	Name        string `json:"name"`
+	Version     string `json:"version"`
+	IsSupported bool   `json:"is_supported"`
 }
 
 //******************************************************************************
@@ -86,70 +85,70 @@ const (
 
 //AppUser model
 type AppUser struct {
-	ID                int `sql:",AUTO_INCREMENT" json:"id"`
-	PhoneNumber       string
-	FirstName         string
-	LastName          string
-	Gender            string
-	Birthday          string
-	Email             string
-	Address           []Address
-	FavouriteProducts []int `pg:",array"`
-	IsComplete        bool
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
-	DeletedAt         time.Time
+	ID                int       `sql:",AUTO_INCREMENT" json:"id"`
+	PhoneNumber       string    `json:"phone_number"`
+	FirstName         string    `json:"first_name"`
+	LastName          string    `json:"last_name"`
+	Gender            string    `json:"gender"`
+	Birthday          string    `json:"birthday"`
+	Email             string    `json:"email"`
+	Address           []Address `json:"address"`
+	FavouriteProducts []int     `pg:",array" json:"favourite_products"`
+	IsComplete        bool      `json:"is_complete"`
+	CreatedAt         time.Time `json:"create_at"`
+	UpdatedAt         time.Time `json:"update_at"`
+	DeletedAt         time.Time `json:"deleted_at"`
 }
 
 //ShopUser model
 type ShopUser struct {
-	ID          int `sql:",AUTO_INCREMENT" json:"id"`
-	PhoneNumber string
-	FirstName   string
-	LastName    string
-	Gender      string
-	Birthday    string
-	Email       string
-	Address     []Address
-	IsComplete  bool
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   time.Time
+	ID          int       `sql:",AUTO_INCREMENT" json:"id"`
+	PhoneNumber string    `json:"phone_number"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	Gender      string    `json:"gender"`
+	Birthday    string    `json:"birthday"`
+	Email       string    `json:"email"`
+	Address     []Address `json:"address"`
+	IsComplete  bool      `json:"is_complete"`
+	CreatedAt   time.Time `json:"create_at"`
+	UpdatedAt   time.Time `json:"update_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
 }
 
 //SuperUser model
 type SuperUser struct {
-	ID          int `sql:",AUTO_INCREMENT" json:"id"`
-	PhoneNumber string
-	FirstName   string
-	LastName    string
-	Gender      string
-	Birthday    string
-	Email       string
-	Address     []Address
-	IsComplete  bool
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   time.Time
+	ID          int       `sql:",AUTO_INCREMENT" json:"id"`
+	PhoneNumber string    `json:"phone_number"`
+	FirstName   string    `json:"first_name"`
+	LastName    string    `json:"last_name"`
+	Gender      string    `json:"gender"`
+	Birthday    string    `json:"birthday"`
+	Email       string    `json:"email"`
+	Address     []Address `json:"address"`
+	IsComplete  bool      `json:"is_complete"`
+	CreatedAt   time.Time `json:"create_at"`
+	UpdatedAt   time.Time `json:"update_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
 }
 
 //DebitcardUser model.
 type DebitcardUser struct {
-	ID          int `sql:",AUTO_INCREMENT" json:"id"`
-	Number      int
-	ShopID      int
-	UserID      int
-	Credit      int
-	Active      bool
-	AssighnDate time.Time
+	ID          int       `sql:",AUTO_INCREMENT" json:"id"`
+	Number      int       `json:"number"`
+	ShopID      int       `json:"shop_id"`
+	UserID      int       `json:"user_id"`
+	Credit      int       `json:"credit"`
+	Active      bool      `json:"active"`
+	AssighnDate time.Time `json:"assighn_date"`
 }
 
 // AdminPanelUser model.
 type AdminPanelUser struct {
-	ID       int `sql:",AUTO_INCREMENT" json:"id"`
-	Username string
-	Password string
-	RealName string
+	ID       int    `sql:",AUTO_INCREMENT" json:"id"`
+	Username string `json:"user_name"`
+	Password string `json:"password"`
+	RealName string `json:"real_name"`
 }
 
 //Mall model
@@ -171,40 +170,40 @@ type Mall struct {
 // MetaProduct model.
 type MetaProduct struct {
 	ID        int `sql:",AUTO_INCREMENT" json:"id"`
-	ProductID int
-	Revision  int
+	ProductID int `json:"product_id"`
+	Revision  int `json:"revision"`
 }
 
 // Product model.
 type Product struct {
-	ID                int `sql:",AUTO_INCREMENT" json:"id"`
-	ShopID            int
-	Name              string
-	Brand             string
-	Code              int
-	Price             int
-	DeliveryTime      int
-	ManufactureSerial string
-	CategoryID        int
-	Discount          int
-	Thumbnail         string
-	Gallery           []Media
-	Gallery3D         Model3D
-	DeliveryPrice     int
-	Guarantee         string
-	Description       string
-	Attributes        map[string]interface{}
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
-	DeletedAt         time.Time
+	ID                int                    `sql:",AUTO_INCREMENT" json:"id"`
+	ShopID            int                    `json:"shop_id"`
+	Name              string                 `json:"name"`
+	Brand             string                 `json:"brand"`
+	Code              int                    `json:"code"`
+	Price             int                    `json:"price"`
+	DeliveryTime      int                    `json:"delivery_time"`
+	ManufactureSerial string                 `json:"manufacture_serial"`
+	CategoryID        int                    `json:"category_id"`
+	Discount          int                    `json:"discount"`
+	Thumbnail         string                 `json:"thumbnail"`
+	Gallery           []Media                `json:"gallery"`
+	Gallery3D         Model3D                `json:"gallery3d"`
+	DeliveryPrice     int                    `json:"delivery_price"`
+	Guarantee         string                 `json:"guarantee"`
+	Description       string                 `json:"description"`
+	Attributes        map[string]interface{} `json:"attributes"`
+	CreatedAt         time.Time              `json:"create_at"`
+	UpdatedAt         time.Time              `json:"update_at"`
+	DeletedAt         time.Time              `json:"deleted_at"`
 }
 
-//ProductAttributes model
-type ProductAttributes struct {
-	ID       int `sql:",AUTO_INCREMENT" json:"id"`
-	Name     string
-	DataType string
-	IsArray  bool
+//ProductAttribute model
+type ProductAttribute struct {
+	ID       int    `sql:",AUTO_INCREMENT" json:"id"`
+	Name     string `json:"name"`
+	DataType string `json:"date_type"`
+	IsArray  bool   `json:"is_array"`
 }
 
 //******************************************************************************
@@ -212,52 +211,53 @@ type ProductAttributes struct {
 // MetaShop model.
 type MetaShop struct {
 	ID       int `sql:",AUTO_INCREMENT" json:"id"`
-	ShopID   int
-	Revision int
+	ShopID   int `json:"shop_id"`
+	Revision int `json:"revision"`
 }
 
 //Shop model
 type Shop struct {
-	ID           int `sql:",AUTO_INCREMENT" json:"id"`
-	MallID       int
-	ContractID   int
-	ShopName     string
-	Brand        string
-	Owner        string
-	Keeper       string
-	VirAddress   int // TODO: format 311 floor 3 number 11
-	Adress       Address
-	PhoneNumbers []string `pg:",array"`
-	Thumbnail    Media    // TODO: create from on file chossen by user
-	MediaGallery []Media  // TODO: hard disk format (by name or by directory hierarchical)
-	Description  string
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	DeletedAt    time.Time
+	ID           int       `sql:",AUTO_INCREMENT" json:"id"`
+	MallID       int       `json:"mall_id"`
+	ContractID   int       `json:"contract_id"`
+	ShopName     string    `json:"shop_name"`
+	Brand        string    `json:"brand"`
+	Owner        string    `json:"owner"`
+	Keeper       string    `json:"keeper"`
+	VirAddress   string    `json:"vir_address"` // TODO: format f01-n11 floor 3 number 11
+	Adress       Address   `json:"address"`
+	PhoneNumbers []string  `pg:",array" json:"phone_number"`
+	Thumbnail    Media     `json:"thumbnail"`     // TODO: create from on file chossen by user
+	MediaGallery []Media   `json:"media_gallery"` // TODO: hard disk format (by name or by directory hierarchical)
+	Description  string    `json:"description"`
+	CreatedAt    time.Time `json:"create_at"`
+	UpdatedAt    time.Time `json:"update_at"`
+	DeletedAt    time.Time `json:"deleted_at"`
 }
 
 //Category model
 type Category struct {
-	ID        int `sql:",AUTO_INCREMENT" json:"id"`
-	Name      string
-	Thumbnail Media
+	ID        int    `sql:",AUTO_INCREMENT" json:"id"`
+	Name      string `json:"name"`
+	Thumbnail Media  `json:"thumbnail"`
 }
 
 //******************************************************************************
 
 //FIXME: add 4 field for %
-//Contracts model (contracts are immutible)
-type Contracts struct {
-	ID             int `sql:",AUTO_INCREMENT" json:"id"`
-	Number         int
-	ContractScan   []Media
-	ContracrtType  string //pysical or virtual
-	CompaneyName   string
-	Date           time.Time
-	BankAccount    []BankAccount
-	Duration       time.Duration
-	MontlytDueDate time.Time
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
-	DeletedAt      time.Time
+
+//Contract model (contracts are immutible)
+type Contract struct {
+	ID             int           `sql:",AUTO_INCREMENT" json:"id"`
+	Number         int           `json:"number"`
+	ContractScan   []Media       `json:"contract_scan"`
+	ContracrtType  string        `json:"contract_type"` //pysical or virtual
+	CompaneyName   string        `json:"companey_name"`
+	Date           time.Time     `json:"date"`
+	BankAccount    []BankAccount `json:"bank_account"`
+	Duration       int           `json:"duration"` //number of month
+	MontlytDueDate time.Time     `json:"montly_due_date"`
+	CreatedAt      time.Time     `json:"create_at"`
+	UpdatedAt      time.Time     `json:"update_at"`
+	DeletedAt      time.Time     `json:"deleted_at"`
 }
